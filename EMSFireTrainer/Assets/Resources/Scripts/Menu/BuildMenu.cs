05d5d9812208fec9c5607db984eebeade2b84459
JippeJoosten <Jippe.joosten@me.com>
2014-05-06 13:04:56 +0200

    Added first Button Visual
    Added Script to add the first button on the stage.
    Added a new font.
    Change the button to the right font size and style.

--- EMSFireTrainer/Assets/Resources/Scripts/Menu/BuildMenu.cs
+++ EMSFireTrainer/Assets/Resources/Scripts/Menu/BuildMenu.cs
@@ -6,20 +6,46 @@ using System.IO;
 
 public class BuildMenu : MonoBehaviour {
 
+	private static Font MenuFont;
+
 	private static GameObject PlayButton;
 	private static GUIText PlayButtonText;
 
 	void Awake() {
 		print("Start!");
+		//Init Font
+		MenuFont = Resources.Load("Fonts/HelveticaNeue") as Font;
 		//Init MainMenu Buttons
 		PlayButton = new GameObject();
-		PlayButtonText = new GUIText();
 
 		//Give Buttons a Name
-		PlayButton.name = "Play Button";
+		PlayButton.name = "Play_Button";
 
 		//Give Buttons the right components
-		PlayButton.AddComponent(PlayButtonText);
+		PlayButton.AddComponent<GUIText>();
+
+		//Select the Components a variable
+		PlayButtonText = PlayButton.GetComponent<GUIText>();
+
+		//Give Components right values
+		//Text
+		PlayButtonText.text = "Play";
+
+		//Posistion
+		PlayButton.transform.position = new Vector3( 0, 1, 0);
+
+		//Color
+		PlayButtonText.color = Color.white;
+
+		//Font
+		PlayButtonText.font = MenuFont;	
+
+		//Font Style
+		PlayButtonText.fontStyle = FontStyle.Bold;
+
+		//Font Size
+		PlayButtonText.fontSize = 50;
+
 
 	}
 
