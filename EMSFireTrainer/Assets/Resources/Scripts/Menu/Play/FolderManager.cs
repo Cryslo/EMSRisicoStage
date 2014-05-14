49893abe34cab43f9f799aa8f3ed5fcf9191dce1
JippeJoosten <Jippe.joosten@me.com>
2014-05-14 11:16:20 +0200

    Updated Folder system

--- EMSFireTrainer/Assets/Resources/Scripts/Menu/Play/FolderManager.cs
+++ EMSFireTrainer/Assets/Resources/Scripts/Menu/Play/FolderManager.cs
@@ -4,39 +4,34 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
-
-public class FolderManager : MonoBehaviour {
+public class FolderManager : MonoBehaviour
+{
 	
 	private string directoryPath;
 	private string savedDirectoryPath;
 	private string savedBackgroundsPath;
-	
 	private string exampleDirectoryPath;
 	private string exampleBackgroundsPath;
-	
 	private int addedBeforeFolders = 1;
-	
 	private string exampleDirectoryName = "Examples";
 	private string savedDirectoryName = "Projects";
 	private string savedBackgroundsName = "Backgrounds";
-	
 	private DirectoryInfo BgFolder;
 	private DirectoryInfo DirectoryFolder;
 	private DirectoryInfo exampleDirectoryFolder;
-	
 	private List<DirectoryInfo> folders;
-	public List<DirectoryInfo> Folders
-	{
-		get{return this.folders;}
+
+	public List<DirectoryInfo> Folders {
+		get{ return this.folders;}
 	}
 	
-	
 	public string SavedBackgroundsPath {
-		get{return this.savedBackgroundsPath;}
+		get{ return this.savedBackgroundsPath;}
 	}
 	
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		// Setting the directoryPath to the assets folder
 		this.directoryPath = Application.persistentDataPath;
 		
@@ -47,23 +42,24 @@ public class FolderManager : MonoBehaviour {
 		// Setting the savedBackgroundsPath to the folder where all Backgrounds are stored
 		this.savedBackgroundsPath = directoryPath + "/" + savedBackgroundsName;
 		// Setting the Directory folder to an variable
-		this.DirectoryFolder = new DirectoryInfo (savedDirectoryPath);
-		Debug.Log (DirectoryFolder.FullName);
+		this.DirectoryFolder = new DirectoryInfo(savedDirectoryPath);
+		Debug.Log(DirectoryFolder.FullName);
 		BuildFolderArray();
 	}
 	
-	public List<DirectoryInfo> GetExamples() {
+	public List<DirectoryInfo> GetExamples()
+	{
 		List<DirectoryInfo> exampleList = new List<DirectoryInfo>();
-		exampleDirectoryFolder = new DirectoryInfo (exampleDirectoryPath);
+		exampleDirectoryFolder = new DirectoryInfo(exampleDirectoryPath);
 		
-		if (exampleDirectoryFolder.Exists) {
+		if(exampleDirectoryFolder.Exists) {
 			
 			//Build Folder Array
-			exampleList = new List<DirectoryInfo> ();
+			exampleList = new List<DirectoryInfo>();
 			
 			DirectoryInfo[] directoryItems = exampleDirectoryFolder.GetDirectories();
 			
-			foreach (DirectoryInfo folder in directoryItems)
+			foreach(DirectoryInfo folder in directoryItems)
 				exampleList.Add(folder);
 			
 			return exampleList;
@@ -73,16 +69,17 @@ public class FolderManager : MonoBehaviour {
 	}
 	
 	// Use this for building the array of folders
-	bool BuildFolderArray() {
+	bool BuildFolderArray()
+	{
 		
-		if (DirectoryFolder.Exists) {
+		if(DirectoryFolder.Exists) {
 			
 			//Build Folder Array
-			folders = new List<DirectoryInfo> ();
+			folders = new List<DirectoryInfo>();
 			
 			DirectoryInfo[] directoryItems = DirectoryFolder.GetDirectories();
 			
-			foreach (DirectoryInfo folder in directoryItems)
+			foreach(DirectoryInfo folder in directoryItems)
 				folders.Add(folder);
 			
 			Debug.Log("Folders: " + directoryItems.Length);
@@ -97,50 +94,66 @@ public class FolderManager : MonoBehaviour {
 	}
 	
 	// Use this for rebuilding the array of folders
-	public bool RebuildFolderArray() {
+	public bool RebuildFolderArray()
+	{
 		//Build Folder Array
-		folders = new List<DirectoryInfo> ();
+		folders = new List<DirectoryInfo>();
 		
 		DirectoryInfo[] directoryItems = DirectoryFolder.GetDirectories();
 		
-		foreach (DirectoryInfo folder in directoryItems)
+		foreach(DirectoryInfo folder in directoryItems)
 			folders.Add(folder);
 		
 		return false;
 	}
 	
 	// Use this for getting all folders made by the user
-	public List<DirectoryInfo> GetFolders () {
+	public List<DirectoryInfo> GetFolders()
+	{
 		List<DirectoryInfo> TempFolders = new List<DirectoryInfo>();
 		
 		DirectoryInfo[] directoryItems = DirectoryFolder.GetDirectories();
-		foreach (DirectoryInfo folder in directoryItems)
+		foreach(DirectoryInfo folder in directoryItems)
 			TempFolders.Add(folder);
 		
 		return TempFolders;
 	}
 	
 	// Use this for get al saved scenes in a project folder
-	public List<FileInfo> GetScenesByName (string FolderName) {
+	public List<FileInfo> GetScenesByName(string FolderName)
+	{
 		List<FileInfo> TempScenes = new List<FileInfo>();
 		DirectoryInfo SelectedFolder;
-		if(Directory.Exists(DirectoryFolder.FullName + "/" + FolderName))
-		{
-			SelectedFolder = new DirectoryInfo (DirectoryFolder.FullName + "/" + FolderName);
-		}else{
-			SelectedFolder = new DirectoryInfo (exampleDirectoryFolder.FullName + "/" + FolderName);
+		if(Directory.Exists(DirectoryFolder.FullName + "/" + FolderName)) {
+			SelectedFolder = new DirectoryInfo(DirectoryFolder.FullName + "/" + FolderName);
+		} else {
+			SelectedFolder = new DirectoryInfo(exampleDirectoryFolder.FullName + "/" + FolderName);
 		}
 		
 		
 		FileInfo[] directoryItems = SelectedFolder.GetFiles();
-		foreach (FileInfo Item in directoryItems)
+		foreach(FileInfo Item in directoryItems)
 			TempScenes.Add(Item);
 		
 		return TempScenes;
 	}
+
+	public string GetFirstSceneByFolderName(string FolderName)
+	{
+		List<FileInfo> TempScenes = new List<FileInfo>();
+		TempScenes = GetScenesByName(FolderName);
+		if(TempScenes.Count != 0)
+		{
+			return TempScenes[0].Name;
+		}else{
+			return "";
+		}
+	}
+			
 	
 	// Use this for get al saved scenes in a project folder
-	public List<FileInfo> GetScenesByNumber (int FolderNumber) {
+	public List<FileInfo> GetScenesByNumber(int FolderNumber)
+	{
 		List<FileInfo> TempScenes = new List<FileInfo>();
 		
 		
@@ -150,22 +163,23 @@ public class FolderManager : MonoBehaviour {
 	
 	#region Folder Managment
 	// Use this for Creating a file by Path
-	public bool CreateFolderWithPath (string path) {
-		Directory.CreateDirectory (path);
-		RebuildFolderArray ();	
+	public bool CreateFolderWithPath(string path)
+	{
+		Directory.CreateDirectory(path);
+		RebuildFolderArray();	
 		return false;
 	}
 	
 	// Use this for Creating a file by Path
-	public bool CreateFolder () {
-		RebuildFolderArray ();
+	public bool CreateFolder()
+	{
+		RebuildFolderArray();
 		string path;
 		int number = 0;
 		string TempName = "Nameless";
 		
-		for (int i = 0; i < folders.Count; i++) {
-			if(folders[i].Name == TempName)
-			{
+		for(int i = 0; i < folders.Count; i++) {
+			if(folders[i].Name == TempName) {
 				number ++;
 				TempName = "Nameless " + number;
 				i = 0;
@@ -173,59 +187,65 @@ public class FolderManager : MonoBehaviour {
 		}
 		
 		path = savedDirectoryPath + "/" + TempName;
-		Directory.CreateDirectory (path);
-		RebuildFolderArray ();	
+		Directory.CreateDirectory(path);
+		RebuildFolderArray();	
 		return false;
 	}
 	
 	
 	// Use this for Creating a file by Name
-	public bool CreateFolderWithName (string name) {
+	public bool CreateFolderWithName(string name)
+	{
 		string path;
 		path = savedDirectoryPath + "/" + name;
-		Directory.CreateDirectory (path);
-		RebuildFolderArray ();	
+		Directory.CreateDirectory(path);
+		RebuildFolderArray();	
 		return false;
 	}
 	
 	// Use this for Delete a file by Path
-	public bool DeleteFolderWithPath (string path) {
+	public bool DeleteFolderWithPath(string path)
+	{
 		
 		return false;
 	}
 	
 	// Use this for Delete a file by Name
-	public	bool DeleteFolderWithName (string name) {
+	public	bool DeleteFolderWithName(string name)
+	{
 		string path;
 		path = savedDirectoryPath + "/" + name;
-		Directory.Delete (path, true);
-		RebuildFolderArray ();	
+		Directory.Delete(path, true);
+		RebuildFolderArray();	
 		return false;
 	}
 	
 	// Use this for Edit a file by Path
-	public bool RenameFolderWithPath (string path) {
+	public bool RenameFolderWithPath(string path)
+	{
 		
 		return false;
 	}
 	
 	// Use this for Edit a file by Name
-	public bool RenameFolderWithID (int id, string newName, int idOffset) {
-		RebuildFolderArray ();
+	public bool RenameFolderWithID(int id, string newName, int idOffset)
+	{
+		RebuildFolderArray();
 		int idPath = id - idOffset;
 		
 		string newPath = savedDirectoryPath + "/" + newName;
 		string oldPath = folders[idPath].FullName;
 		
 		if(newPath != oldPath)
-			Directory.Move (oldPath, newPath);
+			Directory.Move(oldPath, newPath);
 		
 		return false;
 	}
 	#endregion
 	
 	// Use this to get how many backgrounds are in the folder
-	public int GetBackgroundNumbers () {
+	public int GetBackgroundNumbers()
+	{
 		int tempNumber;
 		
 		BgFolder = new DirectoryInfo(savedBackgroundsPath);
@@ -236,24 +256,26 @@ public class FolderManager : MonoBehaviour {
 	}
 	
 	// Use this for getting a background by number
-	public Texture2D GetBackgroundsByNumber(int BG_Number) {
-		Texture2D tempTexture = new Texture2D(0,0);
+	public Texture2D GetBackgroundsByNumber(int BG_Number)
+	{
+		Texture2D tempTexture = new Texture2D(0, 0);
 		
-		tempTexture.LoadImage (File.ReadAllBytes (savedBackgroundsPath + "/" + BG_Number + ".jpg"));
+		tempTexture.LoadImage(File.ReadAllBytes(savedBackgroundsPath + "/" + BG_Number + ".jpg"));
 		
 		return tempTexture;
 	}
 	
-	public List<Texture2D> GetBackgrounds() {
+	public List<Texture2D> GetBackgrounds()
+	{
 		List<Texture2D> tempBGList = new List<Texture2D>();
 		Texture2D tempTexture;
 		
-		DirectoryInfo SelectedFolder = new DirectoryInfo (savedBackgroundsPath);
+		DirectoryInfo SelectedFolder = new DirectoryInfo(savedBackgroundsPath);
 		
 		FileInfo[] directoryItems = SelectedFolder.GetFiles("*.jpg");
-		foreach (FileInfo Item in directoryItems) {
-			tempTexture = new Texture2D(0,0);
-			tempTexture.LoadImage (File.ReadAllBytes(Item.FullName));
+		foreach(FileInfo Item in directoryItems) {
+			tempTexture = new Texture2D(0, 0);
+			tempTexture.LoadImage(File.ReadAllBytes(Item.FullName));
 			tempBGList.Add(tempTexture);
 		}
 		
@@ -262,11 +284,12 @@ public class FolderManager : MonoBehaviour {
 	}
 	
 	// Use this for getting a background by number
-	public Texture2D GetRandomBackgrounds() {
-		Texture2D tempTexture = new Texture2D(0,0);
+	public Texture2D GetRandomBackgrounds()
+	{
+		Texture2D tempTexture = new Texture2D(0, 0);
 		int numberBackground;
 		
-		numberBackground = Random.Range (0, GetBackgroundNumbers());
+		numberBackground = Random.Range(0, GetBackgroundNumbers());
 		
 		//tempTexture.LoadImage (File.ReadAllBytes (savedBackgroundsPath + "/" + 1 + ".jpg"));
 		
@@ -274,11 +297,12 @@ public class FolderManager : MonoBehaviour {
 	}
 	
 	// Use this for getting a background by name
-	public Texture2D GetBackgroundsByName(string name) {
-		Texture2D tempTexture = new Texture2D(0,0);
+	public Texture2D GetBackgroundsByName(string name)
+	{
+		Texture2D tempTexture = new Texture2D(0, 0);
 		
 		
-		tempTexture.LoadImage (File.ReadAllBytes (savedBackgroundsPath + "/" + name + ".jpg"));
+		tempTexture.LoadImage(File.ReadAllBytes(savedBackgroundsPath + "/" + name + ".jpg"));
 		
 		return tempTexture;
 	}
@@ -291,56 +315,49 @@ public class FolderManager : MonoBehaviour {
 		int number = 0;
 		string TempName = name + "(Copy)";
 		
-		for (int i = 0; i < folders.Count; i++) {
-			if(folders[i].Name == TempName)
-			{
+		for(int i = 0; i < folders.Count; i++) {
+			if(folders[i].Name == TempName) {
 				number ++;
 				TempName = name + " (Copy " + number + ")";
 				i = 0;
 			}
 		}
-		Debug.Log ("Temp Name " + TempName);
+		Debug.Log("Temp Name " + TempName);
 		path = savedDirectoryPath + "/" + name;
 		newPath = savedDirectoryPath + "/" + TempName;
 		
 		DirectoryCopy(path, newPath, true);
-		RebuildFolderArray ();
+		RebuildFolderArray();
 	}
-	
 	
 	public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
 	{
-		Debug.Log ("destDirName: " + destDirName);
+		Debug.Log("destDirName: " + destDirName);
 		// Get the subdirectories for the specified directory.
 		DirectoryInfo dir = new DirectoryInfo(sourceDirName);
 		DirectoryInfo[] dirs = dir.GetDirectories();
 		
-		if (!dir.Exists)
-		{
+		if(!dir.Exists) {
 			throw new DirectoryNotFoundException(
 				"Source directory does not exist or could not be found: "
 				+ sourceDirName);
 		}
 		
 		// If the destination directory doesn't exist, create it. 
-		if (!Directory.Exists(destDirName))
-		{
+		if(!Directory.Exists(destDirName)) {
 			Directory.CreateDirectory(destDirName);
 		}
 		
 		// Get the files in the directory and copy them to the new location.
 		FileInfo[] files = dir.GetFiles();
-		foreach (FileInfo file in files)
-		{
+		foreach(FileInfo file in files) {
 			string temppath = destDirName + "/" + file.Name;
 			file.CopyTo(temppath, false);
 		}
 		
 		// If copying subdirectories, copy them and their contents to new location. 
-		if (copySubDirs)
-		{
-			foreach (DirectoryInfo subdir in dirs)
-			{
+		if(copySubDirs) {
+			foreach(DirectoryInfo subdir in dirs) {
 				string temppath = destDirName + "/" + subdir.Name;
 				DirectoryCopy(subdir.FullName, temppath, copySubDirs);
 			}
